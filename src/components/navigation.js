b3515e1622c96c66c0f5c55e4b77c5c915286deb
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2021-08-14 18:07:15 +0530

    Change components to match our content
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,11 +1,10 @@
-import React from "react"
-import {Link} from "gatsby"
-import ThemeChanger from "../components/themeChanger"
+import React from "react";
+import { Link } from "gatsby";
+import ThemeChanger from "../components/themeChanger";
 
-export default (props) => (
-  <nav className="navigation"> 
-    <Link to="/contact">Contact</Link>
-    <ThemeChanger/>
+export default props => (
+  <nav className="navigation">
+    <Link to="/faq">FAQ</Link>
+    <ThemeChanger />
   </nav>
-  
-)
+);
