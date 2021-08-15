ac2abbf37109c24d9d4f4511294b2e92af174597
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2021-08-15 16:59:41 +0530

    Add emphasis to answers
    Co-Authored-By: hyPnOtICDo0g <44530973+hyPnOtICDo0g@users.noreply.github.com>
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -2,9 +2,6 @@ import React from "react";
 import Styles from "./FAQ.module.scss";
 
 class FAQ extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   state = {
     visible: -1
   };
@@ -22,12 +19,12 @@ class FAQ extends React.Component {
         ),
         answer: (
           <div className={Styles.answer}>
-            The main advantage of starting your investments at this point of
-            life is to have more time for getting the benefits of compounding
-            and free to take higher risks with a monthly salary. If you start
-            investing from 20 years of age till you are 30 years old, then there
-            are more chances of you not facing the dearth of money, but you will
-            have to invest consistently for 10 years.
+            The main advantage of investing at this point of life is to obtain
+            the benefits of <strong>compounding</strong> and be free to take
+            higher risks with a monthly salary. If you start investing from the
+            age of 20 until you&#39;re 30, there are more chances of you{" "}
+            <strong>not facing the dearth</strong> of money, but you&#39;ll have
+            to invest <strong>consistently for the next 10 years</strong>.
           </div>
         )
       },
@@ -37,57 +34,63 @@ class FAQ extends React.Component {
         ),
         answer: (
           <div className={Styles.answer}>
-            Stocks are simply the shares that are in the company. Share means
-            one certain part of the company. The bigger the company is, the
-            larger shares will be there, And every share will have a cost. Thus
-            you are buying a part of the company, and you have faith in it. So
-            let’s assume that you have an Internet and mobile connection of Jio,
-            and you feel that Jio has made your life easier. Therefore, you feel
-            that Jio is an excellent company, you know you are a customer of Jio
-            and there are a lot of people who are using their services, so you
-            think “Why don’t I buy some shares of this company?” Then in this
-            way, you are showing confidence in the company. And this is stock
-            Investment. It is your commitment to a certain company and its
-            future.
+            Stocks are simply the shares a company has at present.
+            <br />
+            A share is a certain part of the company. The bigger the company is,
+            the larger shares are present. <br />
+            Every share has a <strong>price</strong>. You need have faith in the
+            company&#39;s <strong>growth</strong>, since you&#39;ll be buying a
+            part of it.
+            <br />
+            So let&#39;s assume that you own couple of Jio &#39;s products &amp;
+            you&#39;re pretty satisfied with it. You feel Jio has made your life
+            easier. You know that being a customer who&#39;s satisfied with
+            their services and aware that there are a lot more people out there
+            who are using their services. So now you feel,
+            <strong>
+              &quot;Why don&#39;t I buy some of Jio&#39;s shares?&quot;
+            </strong>
+            . This is called as a <strong>stock investment</strong>. You
+            invested in Jio since you&#39;re confident about it&#39;s growth
+            &amp; commited to it and it&#39;s future.
           </div>
         )
       },
       {
         question: (
           <div className={Styles.question}>
-            What does buying stocks mean? Why DEMAT account?
+            What does buying stocks mean? What is a DEMAT account?
           </div>
         ),
         answer: (
           <div className={Styles.answer}>
-            Today, everything is Digital. And you hold stocks which you buy in
-            something called a DEMAT account. So a DEMAT account has all your
-            stocks or shares which you have purchased from different companies.
-            So you need a DEMAT account to buy and sell stocks.
+            We&#39;re living in a digital world. Stocks can be bought and sold
+            digitally. Your stocks or shares which you have{" "}
+            <strong>purchased from different companies</strong> are held in
+            something called a <strong>DEMAT</strong> account. Therefore, you
+            need a DEMAT account to buy and sell stocks.
           </div>
         )
       },
       {
         question: (
           <div className={Styles.question}>
-            Which stocks should I pay to buy? How many should I buy? How long
-            should I keep?
+            What stocks to buy? How many should I buy? How long should I keep
+            them?
           </div>
         ),
         answer: (
           <div className={Styles.answer}>
-            Hmm. so these are all the natural questions an early investor has
-            with the stock market. And these can be extremely intimidating.
-            There are a lot of strategies one might use while doing stock
-            marketing. There is this one thing you need to understand, if you
-            want to be really good at it, it does require a lot of time. And
-            there are people who do this full-time. For some people, this is
-            their profession to invest in the stock market, and they come up
-            with their own strategies of how much to spend, what should be the
-            quantity and how long it should be kept. Now, you won’t be able to
-            think about them because you have your own job, own things to do,
-            and I totally get that. So you can do what’s already there in the
-            market, that is Mutual Funds.
+            These are all the natural questions an early investor has regarding
+            the stock market, and these can be extremely intimidating. There are
+            a lot of strategies one might use while trading in the stock market.
+            If you want to understand it quite a bit, it requires a lot of
+            time&amp;experience.
+            <br />
+            There are people who do this full-time. It&#39;s their profession to
+            invest in the stock market, and come up with{" "}
+            <strong>strategies</strong> on how to spend &amp; how long it should
+            be kept.
           </div>
         )
       },
@@ -95,20 +98,36 @@ class FAQ extends React.Component {
         question: <div className={Styles.question}>What are mutual funds?</div>,
         answer: (
           <div className={Styles.answer}>
-            Mutual funds are a portfolio of Stock, which is being managed by
-            someone, lets name him fund manager, and that fund manager has a
-            target of return which is the return that you get on your money, and
-            because of that he buys a group of shares or stocks, and you invest
-            in the fund manager’s stock portfolio, and based on his expertise,
-            his analysis and research team, he invests in those stocks. Now,
-            mutual funds have been there for a long time. And hence there are
-            few cons about it. Firstly,They are not Transparent, which means the
-            stocks belong to someone else. So in your account you will only see
-            some units of the mutual fund, which you buy. so , instead of buying
-            stocks or shares of a company, you are buying units of a mutual
-            fund. Secondly, you have to pay fees called fund fees or a manager’s
-            fees as your money is being managed by someone else. So there is
-            always some money which gets deducted when investing.
+            Mutual funds are <strong>portfolio of a stock</strong>, which is
+            being managed by someone, say, a <strong>Fund Manager</strong>. They
+            have a <strong>target</strong> of return which is the{" "}
+            <strong>return</strong> that you get on your investment, and because
+            of that they <strong>purchases</strong> a group of shares or stocks.
+            Next, you invest in their stock portfolio and based on their{" "}
+            <strong>expertise</strong>, <strong>analysis</strong> and{" "}
+            <strong>research</strong> team, they invest in those stocks.
+            <br />
+            Now, mutual funds have been there for a long time. Hence there are
+            few cons about it.
+            <ul>
+              <li>
+                They&#39;re not transparent, which means the{" "}
+                <strong>stocks belong to someone else</strong>. So, you&#39;re
+                going to see only some units of the mutual fund which you buy in
+                your account. So{" "}
+                <strong>
+                  instead of purchasing stocks or shares of a company you are
+                  buying units of a mutual fund
+                </strong>
+                .
+              </li>
+              <li>
+                You have to pay a fee called fund fee or a manager&#39;s fee,
+                since your money is being{" "}
+                <strong>managed by someone else</strong>. So it&#39;s obvious
+                that some money is deducted during the investment.
+              </li>
+            </ul>
           </div>
         )
       },
@@ -117,34 +136,38 @@ class FAQ extends React.Component {
         answer: (
           <div className={Styles.answer}>
             Inflation is the measurement of the rate at which the price of
-            substances is increasing. Let's say you are in your 20s and you want
-            to retire in 50 years and you aim to reach a position where you have
-            enough money that you don’t have to work anymore, your children have
-            good education facilities and their responsibilities are taken care
-            of. For that you need an amount. And to reach this achievement, only
-            rate of return is not enough, but what’s equally important is what
-            will be the value of that money at some particular instance. Hence,
-            you have to look at inflation-adjusted rate of return and not just
-            the rate of return.
+            substances is increasing. Let&#39;s say you are in your 20&#39;s
+            &amp; you want to retire at the age of 50 and you aim to reach a
+            position where you have enough money that you don&#39;t have to work
+            anymore. Your children have good education facilities and their
+            responsibilities are taken care of.
+            <br />
+            To reach this level, only rate of return is not enough, but
+            what&#39;s equally important is what will be the{" "}
+            <strong>value of that money at some particular instance</strong>.
+            Hence, you have to look at{" "}
+            <strong>inflation-adjusted rate of return</strong> and not just the
+            rate of return.
           </div>
         )
       },
       {
         question: (
           <div className={Styles.question}>
-            Why don’t you suggest some platforms or ways for my early investing?
+            Why don&#39;t you suggest some platforms or ways for my early
+            investment?
           </div>
         ),
         answer: (
           <div className={Styles.answer}>
-            Okay, so I found something really really interesting, and that’s
-            called smallcase, an independent company connected to Zerodha. And
-            what they do is, think of it like a portfolio of stocks or shares
-            but with no negative of what a mutual fund does. Smallcase is a name
-            given to a group of portfolios, so my personal pick for you is the
-            smallcase called “all-weather investing.” If you want to invest with
-            a low risk, then its the best way to invest. So this small case
-            consists of equity/stocks, debt and gold, a mixed portfolio.
+            There&#39;s a company called <strong>Smallcase</strong>, an
+            independent company connected to <strong>Zerodha</strong>. The idea
+            here is, think of it like a portfolio of stocks or shares without
+            the negatives of mutual fund. Smallcase is a name given to a{" "}
+            <strong>group of portfolios</strong>.Smallcase, sometimes called as{" "}
+            <strong>All Weather Investing</strong>, is the best pick for most
+            individuals at their 20&#39;s. If you&#39;re looking for a low risk
+            investment, then this is the best way to invest.
           </div>
         )
       }
