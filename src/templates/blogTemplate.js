bd7f043892ab3dc52c93161f290ade3938ee919d
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2021-08-16 13:01:13 +0530

    Change font
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -11,6 +11,12 @@ export default function Template({
   const { frontmatter, html } = markdownRemark;
   return (
     <Layout>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap"
+        rel="stylesheet"
+      />
       <Helmet>
         <title>
           {frontmatter.title} | {siteMetadata.title}
